c256aa534d21df9ef5039548bbb1f40e62a4ea46
olaba <olaba@student.matnat.uio.no>
2017-09-19 14:19:55 +0200

    Plottet litt til oppgave 1d
    Plottet

--- Oblig1.py
+++ Oblig1.py
@@ -7,8 +7,9 @@ Created on Mon Sep 18 19:43:19 2017
 #Oblig 1 MATINF - 1100
 import numpy as py
 import math as ma
+import matplotlib.pyplot as plt
 
-#Oppgave 1 - a)
+#%% Oppgave 1a)
 #Deklarerer vektor x og de to første indisiene.
 x = py.zeros(100)
 x[0] = 1
@@ -22,13 +23,11 @@ for i in range(2,100):
     print( 2*x[i-1] + 2*x[i-2])
 
 
-print("--------------")
-
-#Oppgave 1 - b)
+#%% Oppgave 1b)
 #Deklarerer vektor x og de to første indisiene.
 x = py.zeros(100)
-x[0] = 1 - ma.sqrt(3)
-x[1] = 2
+x[0] = 1 
+x[1] = 1 - ma.sqrt(3)
     
 #Lager en forløkke over de hundre verdiene
 for i in range(2,100):
@@ -37,5 +36,33 @@ for i in range(2,100):
     x[i] = 2*x[i-1] + 2*x[i-2]
     print( 2*x[i-1] + 2*x[i-2])
     
+#%% Oppgave 1d)
+
+#Lager vektor med løsningen
+xreal = py.zeros(100)
+xreal[0] = 1
+xreal[1] = 1 - ma.sqrt(3)
+for i in range(2,100):
+   xreal[i] = (1 - ma.sqrt(3))**i
+        
+#Regner ut error
+err = py.zeros(100)
+xx = py.zeros(100)
+xx[0] = 1
+for i in range(2,100):
+       err[i] = ma.log10((x[i] - xreal[i]))
+       xx[i] = i
+    
+#Plotter relativ error
+fig1 = plt.figure(2)
+h_10 = plt.plot(err)  
+plt.title("Error - log10")
+plt.ylabel("Error log10")
+plt.xlabel("Step number n")
+plt.grid()
+plt.show()
+fig1.savefig('RelativeError.png')
+    
+#%% 
     
     
