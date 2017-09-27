20706a2a95823a52ea77140a54e85e9e068c2b60
olaba <olaba@student.matnat.uio.no>
2017-09-27 17:46:56 +0200

    Added oppgave 3b
    Wappdidu

--- Oblig1.py
+++ Oblig1.py
@@ -63,6 +63,46 @@ plt.grid()
 plt.show()
 fig1.savefig('RelativeError.png')
     
-#%% 
+#%% Oppgave 2a)
+
+#initialiserer variablene i og n    
+n = [5000.0,100000.0,1000.0]
+i = [4.0,60.0,500.0]
+
+#lager en forløkke over de tre tilfelle
+for t in range (0,3):
+    
+    #initialiserer den første binomialkoeffisienten
+    binom = (i[t]+1.0)
+    
+    #Kalkulerer binomialkoeffisient. Får med siste multiplikasjon ved +1! 
+    for j in range (2,(int(n[t]-i[t]) + 1)):
+        
+        binom = binom*((i[t]+float(j))/float(j))
+
+    print(binom)
+
+
+#%% Oppgave 3b)
+
+from random import random
+
+antfeil = 0; N = 10000
+x0 = y0 = z0 = 0.0
+feil1 = feil2 = 0.0
+
+for i in range(N):
+    x = random(); y = random(); z = random()
+    res1 = x*(y + z)
+    res2 = x*y + y*z
+    
+    if res1 != res2:
+        antfeil += 1
+        x0 = x; y0 = y; z0 = z
+        ikkeass1 = res1
+        ikkeass2 = res2
+        
+print (100. * antfeil/N)
+print (x0, y0, z0, ikkeass1 - ikkeass2)
     
     
